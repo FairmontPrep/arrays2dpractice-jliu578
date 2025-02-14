c11351f2629cd43d56578f43661c09be0288a326
jliu578 <jliu578@students.fairmontschools.com>
2025-02-14 13:59:18 -0800

    Finished!

--- Array.java
+++ Array.java
@@ -7,7 +7,14 @@ public class Array
 	// 7 8 9
 	public int[][] getRowMajorSquare(int x)
 	{
-//	
+		int[][] matrix = new int[x][x];
+		int value = 1;
+		for (int row = 0; row < x; row++) {
+			for (int col = 0; col < x; col++) {
+				matrix[row][col] = value++;
+			}
+		}
+		return matrix;
 	}
 
 	// Array should be of format
@@ -16,16 +23,30 @@ public class Array
 	// 3 6 9
 	public int[][] getColumnMajorSquare(int x)
 	{
-//		
+		int[][] matrix = new int[x][x];
+		int value = 1;
+		for (int col = 0; col < x; col++) {
+			for (int row = 0; row < x; row++) {
+				matrix[col][row] = value++;
+			}
+		}
+		return matrix;
 	}
 
 	// Array should be of format
-	// 1 2 3 4
+	// 1  2  3  4
 	// 5 6 7 8
 	// 9 10 11 12
 	public int[][] getRowMajorRectangle(int row, int column)
 	{
-		
+		int[][] matrix = new int[row][column];
+		int value = 1;
+		for (int r = 0; r < row; r++) {
+			for (int col = 0; col < column; col++) {
+				matrix[r][col] = value++;
+			}
+		}
+		return matrix;
 	}
 
 	// Array should be of format
@@ -34,15 +55,29 @@ public class Array
 	// 3 6 9 12
 	public int[][] getColumnMajorRectangle(int column, int row)
 	{
-	
+		int[][] matrix = new int[column][row];
+		int value = 1;
+		for (int col = 0; col < column; col++) {
+			for (int r = 0; r < row; r++) {
+				matrix[col][r] = value++;
+			}
+		}
+		return matrix;
 	}
 
 	// Please note that each digit occupies two spaces and there is a space between
 	// them.
 	// You are not required to deal with three digit numbers.
-	public void printDouble(int[][] a)
-    {
-	
-
+	public void printDouble(int[][] a) {
+		for (int[] row : a) {
+			for (int num : row) {
+				if (num < 10) {
+					System.out.printf(" %d  ", num);  // Extra space for single-digit numbers
+				} else {
+					System.out.printf("%d  ", num);   // Normal spacing for two-digit numbers
+				}
+			}
+			System.out.println(); // Move to the next line after printing a row
+		}
 	}
 }
